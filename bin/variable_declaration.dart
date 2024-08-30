3f3a1ba5c7ed7ec41a7758ac615a16f9afca5517
Joseph Lord Quaicoe <joselq1122@gmail.com>
2024-08-30 14:47:03 +0000

    data types

---
+++ bin/variable_declaration.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  final myList = [
+    1,
+    4,
+    7,
+  ];
+  myList.add(4);
+  print(myList);
+  const Map<String, String> myMap = {'FOO': 'lORD'};
+  print(myMap['FOO']);
+}
---
+++ test/variable_declaration_test.dart
@@ -0,0 +1,8 @@
+import 'package:variable_declaration/variable_declaration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
